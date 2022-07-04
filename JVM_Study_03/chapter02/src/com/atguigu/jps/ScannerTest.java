39df35cd6fcfb459c74375d17b481bcb9a23f79e
BigShrimpX <1730821268@qq.com>
2022-07-04 23:44:08 +0800

    JVM_Study 2 and 3

---
+++ JVM_Study_03/chapter02/src/com/atguigu/jps/ScannerTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.jps;
+
+import java.util.Scanner;
+
+/**
+ * @author 夏明
+ * @version 1.0
+ */
+public class ScannerTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String info = scanner.next();
+    }
+}
